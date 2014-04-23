37cb916cdc742b3d98e35b696fee547e58a81472
Dan Friedman <dfriedma@oberlin.edu>
2014-04-23 18:18:08 -0400

    Compile generated assembly in test.

--- bpl/test/code_gen_test.py
+++ bpl/test/code_gen_test.py
@@ -1,6 +1,7 @@
 from bpl.parser.parser import Parser
 from bpl.type_checker.type_checker import TypeChecker
 from bpl.code_gen.code_gen import CodeGenerator
+from subprocess import call
 import sys
 
 if __name__ == '__main__':
@@ -10,6 +11,11 @@ if __name__ == '__main__':
             p.parse()
             t = TypeChecker(p.filename, p.tree, DEBUG=True)
             t.type_check()
+            c = CodeGenerator(t.filename, t.tree, DEBUG=True)
+            c.gen_all()
+            prog_name = p.filename[:-4]
+            assembly_name = prog_name + '.s'
+            call(['gcc', '-g', assembly_name, '-o', prog_name])
     else:
         p = Parser('bpl/test/code_gen_example.bpl')
         p.parse()
@@ -17,3 +23,6 @@ if __name__ == '__main__':
         t.type_check()
         c = CodeGenerator(t.filename, t.tree, DEBUG=True)
         c.gen_all()
+        prog_name = p.filename[:-4]
+        assembly_name = prog_name + '.s'
+        call(['gcc', '-g', assembly_name, '-o', prog_name])
